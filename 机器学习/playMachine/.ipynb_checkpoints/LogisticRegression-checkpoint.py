83b1c7c373816d6014badba7db482689f6b1fcf8
Exia <someone741@qq.com>
2019-06-28 07:57:05 +0800

    删除了一些冗余文件

--- 机器学习/playMachine/.ipynb_checkpoints/LogisticRegression-checkpoint.py
+++ 机器学习/playMachine/.ipynb_checkpoints/LogisticRegression-checkpoint.py
@@ -25,11 +25,6 @@ class LogisticRegression:
                 return float('inf')
 
         def dJ(theta, X_b, y):
-            # res = np.empty(len(theta))
-            # res[0] = np.sum(X_b.dot(theta) - y)
-            # for i in range(1, len(theta)):
-            #     res[i] = (X_b.dot(theta) - y).dot(X_b[:, i])
-            # return res * 2 / len(X_b)
             return X_b.T.dot(self._sigmoid(X_b.dot(theta)) - y)  / len(X_b)
 
         def gradient_descent(X_b, y, initial_theta, eta, n_iters=1e4, epsilon=1e-8):
@@ -51,6 +46,42 @@ class LogisticRegression:
         X_b = np.hstack([np.ones((len(X_train), 1)), X_train])
         initial_theta = np.zeros(X_b.shape[1])
         self._theta = gradient_descent(X_b, y_train, initial_theta, eta, n_iters)
+
+        self.intercept_ = self._theta[0]
+        self.coef_ = self._theta[1:]
+
+        return self
+    
+    def fit_sgd(self, X_train, y_train, n_iters=5, t0=5, t1=50):
+        """根据训练数据集X_train, y_train, 使用梯度下降法训练Linear Regression模型"""
+        assert X_train.shape[0] == y_train.shape[0], \
+            "the size of X_train must be equal to the size of y_train"
+        assert n_iters >= 1
+
+        def dJ_sgd(theta, X_b_i, y_i):
+            return X_b_i.T.dot(self._sigmoid(X_b_i.dot(theta)) - y_i)  / len(X_b_i)
+
+        def sgd(X_b, y, initial_theta, n_iters, t0=5, t1=50):
+
+            def learning_rate(t):
+                return t0 / (t + t1)
+
+            theta = initial_theta
+            m = len(X_b)
+
+            for cur_iter in range(n_iters):
+                indexes = np.random.permutation(m)
+                X_b_new = X_b[indexes]
+                y_new = y[indexes]
+                for i in range(m):
+                    gradient = dJ_sgd(theta, X_b_new[i], y_new[i])
+                    theta = theta - learning_rate(cur_iter * m + i) * gradient
+
+            return theta
+
+        X_b = np.hstack([np.ones((len(X_train), 1)), X_train])
+        initial_theta = np.random.randn(X_b.shape[1])
+        self._theta = sgd(X_b, y_train, initial_theta, n_iters, t0, t1)
 
         self.intercept_ = self._theta[0]
         self.coef_ = self._theta[1:]
